526a86744bac9b732e8a8063eb576999aaa83b48
Jamie Iles <jamie@jamieiles.com>
2011-06-23 17:50:40 +0100

    hw: otp_pc30xx: store the OTP data in a file
    Use a file for backing the OTP so that it is persistent across reboots.

--- hw/otp_pc30xx.c
+++ hw/otp_pc30xx.c
@@ -5,6 +5,8 @@
  *
  * This code is licenced under the GPL.
  */
+#include <stdio.h>
+#include "bitmap.h"
 #include "sysbus.h"
 #include "qemu-timer.h"
 #include "qemu-log.h"
@@ -37,13 +39,42 @@ struct pc30xx_otp {
     uint32_t direct_io;
 #define PGM_STATUS_IN_PROG      (1 << 0)
     uint32_t pgm_status;
+    uint32_t io_flags;
+#define IO_FLAG_ROPB_BASE       0
+#define IO_FLAG_LTP_BASE        4
+#define IO_FLAG_GLOBAL_LTP      (1 << 8)
+    unsigned long read_bitmap[BITS_TO_LONGS(4096)];
 
-    uint32_t otp[4096];
+    int backing;
 };
+
+static uint32_t otp_read_word(struct pc30xx_otp *otp, uint32_t word_addr)
+{
+    uint32_t val;
+    ssize_t br;
+
+    lseek(otp->backing, word_addr * sizeof(uint32_t), SEEK_SET);
+    br = read(otp->backing, &val, sizeof(uint32_t));
+    assert(br == sizeof(uint32_t));
+
+    return val;
+}
+
+static void otp_write_word(struct pc30xx_otp *otp, uint32_t word_addr,
+                           uint32_t val)
+{
+    ssize_t bw;
+
+    lseek(otp->backing, word_addr * sizeof(val), SEEK_SET);
+    bw = write(otp->backing, &val, sizeof(val));
+    assert(bw == sizeof(val));
+}
 
 static uint32_t pc30xx_otp_readl(void *opaque, target_phys_addr_t addr)
 {
     struct pc30xx_otp *otp = opaque;
+    uint32_t word_addr = addr / 4;
+    uint32_t sector = addr / 4096;
 
     switch (addr) {
     case 0x4000:
@@ -80,20 +111,44 @@ static uint32_t pc30xx_otp_readl(void *opaque, target_phys_addr_t addr)
         return otp->direct_io;
 
     case 0x0000 ... 0x3fff:
-        return otp->otp[addr / 4];
+        /*
+         * The Read Once Per Boot flags work on 32-bit quantities.  We track
+         * accesses to each word so that it can only be read once.  If the
+         * word has previously been read, return all 1's.
+         */
+        if ((otp->io_flags & (1 << (IO_FLAG_ROPB_BASE + sector))) &&
+            test_and_set_bit(word_addr, otp->read_bitmap))
+                return ~0;
+        return otp_read_word(otp, word_addr);
 
     default:
         return 0;
     }
 }
 
+/*
+ * Check if we can write to the given sector.  If either the global last time
+ * program or the sector last time program is active then it's a no-go.
+ */
+static bool otp_sector_can_write(struct pc30xx_otp *otp, uint32_t sector)
+{
+    return !((otp->io_flags & IO_FLAG_GLOBAL_LTP) ||
+             (otp->io_flags & (1 << (IO_FLAG_LTP_BASE + sector))));
+}
+
 static void otp_prog_done(void *opaque)
 {
     struct pc30xx_otp *otp = opaque;
     uint32_t addr = otp->pgm_addr / 4;
+    uint32_t sector = otp->pgm_addr / 1024;
 
-    otp->otp[addr] |= otp->pgm_datal;
-    otp->otp[addr + 1] |= otp->pgm_datah;
+    if (otp_sector_can_write(otp, sector)) {
+        uint32_t v;
+        v = otp_read_word(otp, addr);
+        otp_write_word(otp, addr, v | otp->pgm_datal);
+        v = otp_read_word(otp, addr + 1);
+        otp_write_word(otp, addr + 1, v | otp->pgm_datah);
+    }
 
     otp->status &= ~STATUS_BUSY;
     otp->pgm_status &= ~PGM_STATUS_IN_PROG;
@@ -184,20 +239,48 @@ static CPUWriteMemoryFunc * const pc30xx_otp_write[] = {
     pc30xx_otp_writel
 };
 
+static void pc30xx_otp_set_io(void *opaque, int irq, int level)
+{
+    struct pc30xx_otp *otp = opaque;
+
+    if (level)
+        otp->io_flags |= (1 << irq);
+    else
+        otp->io_flags &= ~(1 << irq);
+}
+
 static int pc30xx_otp_init(SysBusDevice *dev)
 {
     struct pc30xx_otp *t = FROM_SYSBUS(struct pc30xx_otp, dev);
-    int regs;
+    int regs, err;
+
+    t->backing = open("otp_pc30xx.bin", O_RDWR | O_CREAT, 0644);
+    /* Make sure that all of the OTP exists. */
+    err = ftruncate(t->backing, 16 * 1024);
+    if (err)
+        goto out_close;
 
     regs = cpu_register_io_memory(pc30xx_otp_read, pc30xx_otp_write, t,
 				  DEVICE_NATIVE_ENDIAN);
     sysbus_init_mmio(dev, 0x8000, regs);
+    /*
+     * We have 9 I/O's:
+     *  - 4 x Read Once Per Boot flags
+     *  - 4 x Last Time Program flags
+     *  - 1 x Global Last Time Program flag
+     */
+    qdev_init_gpio_in(&dev->qdev, pc30xx_otp_set_io, 9);
 
     t->bh = qemu_bh_new(otp_prog_done, t);
     t->ptimer = ptimer_init(t->bh);
     ptimer_set_freq(t->ptimer, 1000000);
 
     return 0;
+
+out_close:
+    close(t->backing);
+
+    return err;
 }
 
 static SysBusDeviceInfo pc30xx_otp_info = {
